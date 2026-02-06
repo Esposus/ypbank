da473cc055ca76c28115fb203172ba0b8fe44bb7
Dmitry <adm713934@yandex.ru>
2026-02-06 19:48:00 +0300

    remove ignored files

--- target/debug/deps/ypbank_compare-cf80e772d9d433ca.d
+++
@@ -1,5 +0,0 @@
-/home/user/Rust/Yandex/ypbank/target/debug/deps/ypbank_compare-cf80e772d9d433ca.d: ypbank-compare/src/main.rs
-
-/home/user/Rust/Yandex/ypbank/target/debug/deps/libypbank_compare-cf80e772d9d433ca.rmeta: ypbank-compare/src/main.rs
-
-ypbank-compare/src/main.rs:
--- target/debug/deps/ypbank_parser-3e88e4a3dd74a39b.d
+++
@@ -1,5 +0,0 @@
-/home/user/Rust/Yandex/ypbank/target/debug/deps/ypbank_parser-3e88e4a3dd74a39b.d: ypbank-parser/src/lib.rs
-
-/home/user/Rust/Yandex/ypbank/target/debug/deps/libypbank_parser-3e88e4a3dd74a39b.rmeta: ypbank-parser/src/lib.rs
-
-ypbank-parser/src/lib.rs:
