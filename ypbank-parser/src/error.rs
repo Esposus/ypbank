69e81246c43078bcf252cfdb7e23691804eda09f
user <adm713934@yandex.ru>
2026-02-09 14:57:38 +0300

    added Error module

---
+++ ypbank-parser/src/error.rs
@@ -0,0 +1,29 @@
+use thiserror::Error;
+
+// Тип результата для операций парсинга
+pub type ParseResult<T> = std::result::Result<T, ParseError>;
+
+// Ошибка парсинга финансовых данных
+#[derive(Error, Debug)]
+pub enum ParseError {
+    #[error("IO ошибка: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("UTF-8 ошибка: {0}")]
+    Utf8(#[from] std::string::FromUtf8Error),
+
+    #[error("Ошибка парсинга числа: {0}")]
+    ParseInt(#[from] std::num::ParseIntError),
+
+    #[error("Неверный тип транзакции: {0}")]
+    InvalidTransactionType(String),
+
+    #[error("Неверный статус транзакции: {0}")]
+    InvalidTransactionStatus(String),
+
+    #[error("Магическое число не совпадает")]
+    InvalidMagic,
+
+    #[error("Размер записи не совпадает")]
+    RecordSizeMismatch,
+}
--- ypbank-parser/src/transaction.rs
+++ ypbank-parser/src/transaction.rs
@@ -1,3 +1,4 @@
+use crate::error::ParseError;
 use std::fmt;
 
 // Тип финансовой транзакции
